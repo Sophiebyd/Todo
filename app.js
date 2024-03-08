61d17d23d330c0720941e235a8d2333919ed508f
Sophiebyd <sophiebyd@proton.me>
2024-03-08 10:56:34 +0100

    add event

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const form = document.querySelector('#form');
-const list = document.querySelector('.formList');
-const deleteBtn = document.querySelector('.deleteBtn');
-const content = document.querySelector('#newContent');
+const form = document.querySelector('#form'); // prends tout le formulaire
+const tableBody = document.querySelector('tbody'); // prends le corps du tableau
+const content = document.querySelector('#newContent'); // sélectionne l'input pour ajouter un élément
 
-toDo()
-function toDo() {
+newEvent()
 
+function newEvent() {
+form.addEventListener('submit', addContent);
+}
+
+//Ajout de l'event
+function addContent(event){
+if (content.value === '')
+    alert ('Ajouter un élément')
+    event.preventDefault()
 }
