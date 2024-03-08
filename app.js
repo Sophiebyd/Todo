f618e6f1ee40b20c07dd9c4e3d9ba18419ed1067
Sophiebyd <sophiebyd@proton.me>
2024-03-08 09:59:33 +0100

    Start JS

---
+++ app.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#form');
+const list = document.querySelector('.formList');
+const deleteBtn = document.querySelector('.deleteBtn');
+const content = document.querySelector('#newContent');
+
+toDo()
+function toDo() {
+
+}
