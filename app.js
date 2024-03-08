7cad71fb284083eb146a53f911f8beb7f2d11343
Sophiebyd <sophiebyd@proton.me>
2024-03-08 12:19:05 +0100

    start creating elements

--- app.js
+++ app.js
@@ -12,5 +12,26 @@ form.addEventListener('submit', addContent);
 function addContent(event){
 if (content.value === '')
     alert ('Ajouter un élément')
+
+// ajouter une ligne à la table 
+const tr = document.createElement('tr');
+
+//création des td
+const tdCheckbox = document.createElement('td');
+const tdBadgeText = document.createElement('td');
+const tdText = document.createElement('td');
+const tdBtnDelete = document.createElement('td');
+
+//élément du checkbox
+const checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+checkbox.classList = ('form-check-input', 'form-check-input-lg');
+checkbox.style.width = '2em';
+checkbox.style.height = '2em';
+
+//élément du badge 
+const badge = document.createElement('')
+
+
     event.preventDefault()
 }
