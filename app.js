76fb3e9707c613b64a39015780fbe10343404d7b
Sophiebyd <sophiebyd@proton.me>
2024-03-08 13:03:54 +0100

    add new elements

--- app.js
+++ app.js
@@ -24,14 +24,46 @@ const tdBtnDelete = document.createElement('td');
 
 //élément du checkbox
 const checkbox = document.createElement('input');
+checkbox.classList.add = ('form-check-input', 'form-check-input-lg');
 checkbox.type = 'checkbox';
-checkbox.classList = ('form-check-input', 'form-check-input-lg');
 checkbox.style.width = '2em';
 checkbox.style.height = '2em';
 
 //élément du badge 
-const badge = document.createElement('')
+const badge = document.createElement('span');
+badge.classList.add = ('badge', 'bg-primary');
 
+//élement du petit texte
+const smallText = createElement('small');
+smallText.classList.add = ('form-text', 'text-muted');
+
+//élement de l'input texte readonly
+const text = createElement('input');
+text.type = 'text';
+text.classList.add = ('form-control', 'formList');
+
+//élement du bouton Effacer
+const btnDelete = createElement('button');
+btnDelete.type = 'submit';
+btnDelete.classList.add = ('btn', 'btn-danger');
+
+//placer le bouton effacer à la fin 
+tdBtnDelete.className = 'text-end';
+
+//relier les éléments au td
+tdCheckbox.appendChild(checkbox);
+tdBadgeText.appenchild(badge);
+tdBadgeText.appenchild(smallText);
+tdText.appendChild(text);
+tdBtnDelete.appenchild(btnDelete);
+
+//relier les éléments au tr
+tr.appendChild(checkbox);
+tr.appendChild(badge);
+tr.appendChild(smallText);
+tr.appendChild(text);
+tr.appendChild(btnDelete);
+tableBody.appendChild(tr);
 
     event.preventDefault()
 }
