94953b225994e39b659ee168630809f1c2eee058
Sophiebyd <sophiebyd@proton.me>
2024-03-11 15:43:35 +0100

    add condition for badge

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ function addContent(event){
 if (content.value === '')
     alert ('Ajouter un élément')
 
+//récupérer la priorité
+const priority = document.querySelector('#priority').value;
+
+//récupérer la date
+const date = document.querySelector('#date').value;
+
 // ajouter une ligne à la table 
 const tr = document.createElement('tr');
 
@@ -24,32 +30,43 @@ const tdBtnDelete = document.createElement('td');
 
 //élément du checkbox
 const checkbox = document.createElement('input');
-checkbox.classList.add = ('form-check-input', 'form-check-input-lg');
+checkbox.classList.add ('form-check-input', 'form-check-input-lg');
 checkbox.type = 'checkbox';
 checkbox.style.width = '2em';
 checkbox.style.height = '2em';
 
 //élément du badge 
 const badge = document.createElement('span');
-badge.classList.add = ('badge', 'bg-primary');
-badge.textContent = ('Priorité normale');
+
+if (priority === 'Normale'){
+    badge.classList.add ('badge', 'bg-primary')
+} else if (priority === 'Haute'){
+    badge.classList.add ('badge', 'bg-warning')
+} else {
+    badge.classList.add ('badge', 'bg-danger')
+}
+
+badge.textContent = `${priority}`;
+
+//ajout de la base <br>
+const br = document.createElement('br');
 
 //élement du petit texte
 const smallText = document.createElement('small');
 smallText.classList.add = ('form-text', 'text-muted');
-smallText.textContent = ('pas de date limite');
+smallText.textContent = date;
 
 //élement de l'input texte readonly
 const text = document.createElement('input');
 text.type = ('text');
-text.classList.add = ('form-control', 'formList');
+text.classList.add ('form-control', 'formList');
 text.value = content.value;
 text.readOnly = true;
 
 //élement du bouton Effacer
 const btnDelete = document.createElement('button');
 btnDelete.type = 'submit';
-btnDelete.classList.add = ('btn', 'btn-danger');
+btnDelete.classList.add ('btn', 'btn-danger');
 btnDelete.textContent = ('Effacer');
 
 //placer le bouton effacer à la fin 
@@ -58,6 +75,7 @@ tdBtnDelete.className = 'text-end';
 //relier les éléments au td (cellules)
 tdCheckbox.appendChild(checkbox);
 tdBadgeText.appendChild(badge);
+tdBadgeText.appendChild(br);
 tdBadgeText.appendChild(smallText);
 tdText.appendChild(text);
 tdBtnDelete.appendChild(btnDelete);
