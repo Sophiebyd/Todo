5d45f71eb460ec398fb3be940d91f1d825b8e9d1
Sophiebyd <sophiebyd@proton.me>
2024-03-11 17:25:28 +0100

    filter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#form'); // prends tout le formulaire
 const tableBody = document.querySelector('tbody'); // prends le corps du tableau
 const content = document.querySelector('#newContent'); // sélectionne l'input pour ajouter un élément
+const filter = document.querySelector('#filter'); // sélectionne l'id de l'input pour filtrer 
 
 newEvent()
 
@@ -8,34 +9,37 @@ function newEvent() {
 form.addEventListener('submit', addContent);
 }
 
-//Ajout de l'event
+// Ajout de l'event
 function addContent(event){
 if (content.value === '')
     alert ('Ajouter un élément')
 
-//récupérer la priorité
+// récupérer la priorité
 const priority = document.querySelector('#priority').value;
 
-//récupérer la date
+// récupérer la date
 const date = document.querySelector('#date').value;
+
+// filtrer la liste
+filter.addEventListener('keyup', filterList)
 
 // ajouter une ligne à la table 
 const tr = document.createElement('tr');
 
-//création des td
+// création des td
 const tdCheckbox = document.createElement('td');
 const tdBadgeText = document.createElement('td');
 const tdText = document.createElement('td');
 const tdBtnDelete = document.createElement('td');
 
-//élément du checkbox
+// élément du checkbox
 const checkbox = document.createElement('input');
 checkbox.classList.add ('form-check-input', 'form-check-input-lg');
 checkbox.type = 'checkbox';
 checkbox.style.width = '2em';
 checkbox.style.height = '2em';
 
-//élément du badge 
+// élément du badge 
 const badge = document.createElement('span');
 
 if (priority === 'Normale'){
@@ -48,31 +52,31 @@ if (priority === 'Normale'){
 
 badge.textContent = `${priority}`;
 
-//ajout de la base <br>
+// ajout de la base <br>
 const br = document.createElement('br');
 
-//élement du petit texte
+// élement du petit texte
 const smallText = document.createElement('small');
 smallText.classList.add = ('form-text', 'text-muted');
 smallText.textContent = date;
 
-//élement de l'input texte readonly
+// élement de l'input texte readonly
 const text = document.createElement('input');
 text.type = ('text');
 text.classList.add ('form-control', 'formList');
 text.value = content.value;
 text.readOnly = true;
 
-//élement du bouton Effacer
+// élement du bouton Effacer
 const btnDelete = document.createElement('button');
 btnDelete.type = 'submit';
 btnDelete.classList.add ('btn', 'btn-danger');
 btnDelete.textContent = ('Effacer');
 
-//placer le bouton effacer à la fin 
+// placer le bouton effacer à la fin 
 tdBtnDelete.className = 'text-end';
 
-//relier les éléments au td (cellules)
+// relier les éléments au td (cellules)
 tdCheckbox.appendChild(checkbox);
 tdBadgeText.appendChild(badge);
 tdBadgeText.appendChild(br);
@@ -80,7 +84,7 @@ tdBadgeText.appendChild(smallText);
 tdText.appendChild(text);
 tdBtnDelete.appendChild(btnDelete);
 
-//relier les éléments au tr (lignes)
+// relier les éléments au tr (lignes)
 tr.appendChild(tdCheckbox);
 tr.appendChild(tdBadgeText);
 tr.appendChild(tdText);
@@ -88,4 +92,21 @@ tr.appendChild(tdBtnDelete);
 tableBody.appendChild(tr);
 
     event.preventDefault()
+}
+
+// fonction de filterList
+function filterList (e) {
+    const list = e.target.value.toLowerCase();
+    document.querySelectorAll('tbody').forEach(
+        function (select) {
+            const word = select.firstChild.textContent;
+            if (word.toLocaleLowerCase().indexOf(list) != -1) {
+                select.getElementsByClassName.display = 'block';
+            } else {
+                select.style.display = 'none';
+            }
+        }
+    );
+
+    console.log(list);
 }
