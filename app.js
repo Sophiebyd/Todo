2822a83fcf44c8b764cfd4606048bff5f8cab448
Sophiebyd <sophiebyd@proton.me>
2024-03-22 11:29:56 +0100

    date undefined fixed

--- app.js
+++ app.js
@@ -5,125 +5,139 @@ const filter = document.querySelector("#filter"); // sélectionne l'id de l'inpu
 
 // Ajout de l'event
 function addContent(event) {
-    if (content.value === "") alert("Ajouter un élément");
+  event.preventDefault();
 
-    // récupérer la priorité
-    const priority = document.querySelector("#priority").value;
+  if (content.value === "") alert("Ajouter un élément");
 
-    // récupérer la date et créer l'objet
-    const date = new Date(document.querySelector("#date").value);
+  // récupérer la priorité
+  const priority = document.querySelector("#priority").value;
 
-    // vérifier si la date est dépassée
-    const currentDate = new Date();
-    const datePassed = date < currentDate;
+  let date;
 
-    // ajouter une ligne à la table
-    const tr = document.createElement("tr");
+  // if selector =! vide
+  //  date = new date
 
-    // création des td
-    const tdCheckbox = document.createElement("td");
-    const tdBadgeText = document.createElement("td");
-    const tdText = document.createElement("td");
-    const tdBtnDelete = document.createElement("td");
+  if (document.querySelector("#date").value !== "") {
+    date = new Date(document.querySelector("#date").value);
+  }
 
-    // élément du checkbox
-    const checkbox = document.createElement("input");
-    checkbox.classList.add("form-check-input", "form-check-input-lg");
-    checkbox.type = "checkbox";
-    checkbox.style.width = "2em";
-    checkbox.style.height = "2em";
+  console.log(date);
 
-    // élément du badge
-    const badge = document.createElement("span");
-    badge.textContent = `${priority}`;
+  // vérifier si la date est dépassée
+  const currentDate = new Date();
+  const datePassed = date < currentDate;
 
-    if (priority === "Priorité normale") {
-        tr.dataset.priority = 0;
-        badge.classList.add("badge", "bg-primary");
-    } else if (priority === "Priorité haute") {
-        tr.dataset.priority = 1;
-        badge.classList.add("badge", "bg-warning");
-    } else {
-        tr.dataset.priority = 2;
-        badge.classList.add("badge", "bg-danger");
-    }
+  // ajouter une ligne à la table
+  const tr = document.createElement("tr");
 
-    // ajout de la base <br>
-    const br = document.createElement("br");
+  // création des td
+  const tdCheckbox = document.createElement("td");
+  const tdBadgeText = document.createElement("td");
+  const tdText = document.createElement("td");
+  const tdBtnDelete = document.createElement("td");
 
+  // élément du checkbox
+  const checkbox = document.createElement("input");
+  checkbox.classList.add("form-check-input", "form-check-input-lg");
+  checkbox.type = "checkbox";
+  checkbox.style.width = "2em";
+  checkbox.style.height = "2em";
+
+  // élément du badge
+  const badge = document.createElement("span");
+  badge.textContent = `${priority}`;
+
+  if (priority === "Priorité normale") {
+    tr.dataset.priority = 0;
+    badge.classList.add("badge", "bg-primary");
+  } else if (priority === "Priorité haute") {
+    tr.dataset.priority = 1;
+    badge.classList.add("badge", "bg-warning");
+  } else {
+    tr.dataset.priority = 2;
+    badge.classList.add("badge", "bg-danger");
+  }
+
+  // ajout de la base <br>
+  const br = document.createElement("br");
+
+  const smallText = document.createElement("small");
+  smallText.classList.add("form-text", "text-muted");
+
+  if (date) {
     // élement du petit texte
-    const smallText = document.createElement("small");
-    smallText.classList.add("form-text", "text-muted");
     smallText.textContent = date.toLocaleDateString("fr-FR");
 
     if (datePassed) {
-        smallText.classList.add("badge", "bg-secondary");
+      smallText.classList.add("badge", "bg-secondary");
     }
 
-    // défini le dataset de "date"
     tr.dataset.date = date.getTime();
+  } else {
+    smallText.textContent = "pas de date limite";
+  }
 
-    // élement de l'input texte readonly
-    const text = document.createElement("input");
-    text.type = "text";
-    text.classList.add("form-control", "formList");
-    text.value = content.value;
-    text.readOnly = true;
+  // défini le dataset de "date"
 
-    // élement du bouton Effacer
-    const btnDelete = document.createElement("button");
-    btnDelete.type = "submit";
-    btnDelete.classList.add("btn", "btn-danger");
-    btnDelete.textContent = "Effacer";
+  // élement de l'input texte readonly
+  const text = document.createElement("input");
+  text.type = "text";
+  text.classList.add("form-control", "formList");
+  text.value = content.value;
+  text.readOnly = true;
 
-    // placer le bouton effacer à la fin
-    tdBtnDelete.className = "text-end";
+  // élement du bouton Effacer
+  const btnDelete = document.createElement("button");
+  btnDelete.type = "submit";
+  btnDelete.classList.add("btn", "btn-danger");
+  btnDelete.textContent = "Effacer";
 
-    // relier les éléments au td (cellules)
-    tdCheckbox.appendChild(checkbox);
-    tdBadgeText.appendChild(badge);
-    tdBadgeText.appendChild(br);
-    tdBadgeText.appendChild(smallText);
-    tdText.appendChild(text);
-    tdBtnDelete.appendChild(btnDelete);
+  // placer le bouton effacer à la fin
+  tdBtnDelete.className = "text-end";
 
-    // relier les éléments au tr (lignes)
-    tr.appendChild(tdCheckbox);
-    tr.appendChild(tdBadgeText);
-    tr.appendChild(tdText);
-    tr.appendChild(tdBtnDelete);
-    tableBody.appendChild(tr);
+  // relier les éléments au td (cellules)
+  tdCheckbox.appendChild(checkbox);
+  tdBadgeText.appendChild(badge);
+  tdBadgeText.appendChild(br);
+  tdBadgeText.appendChild(smallText);
+  tdText.appendChild(text);
+  tdBtnDelete.appendChild(btnDelete);
 
-    event.preventDefault();
+  // relier les éléments au tr (lignes)
+  tr.appendChild(tdCheckbox);
+  tr.appendChild(tdBadgeText);
+  tr.appendChild(tdText);
+  tr.appendChild(tdBtnDelete);
+  tableBody.appendChild(tr);
 }
 
 // fonction de filterList
 function filterList(e) {
-    const list = e.target.value.toLowerCase();
+  const list = e.target.value.toLowerCase();
 
-    document.querySelectorAll(".formList").forEach(function (select) {
-        const word = select.value.toLowerCase();
-        const parent = select.closest("tr");
+  document.querySelectorAll(".formList").forEach(function (select) {
+    const word = select.value.toLowerCase();
+    const parent = select.closest("tr");
 
-        if (word.indexOf(list) !== -1) {
-            parent.style.display = null;
-        } else {
-            parent.style.display = "none";
-        }
-    });
+    if (word.indexOf(list) !== -1) {
+      parent.style.display = null;
+    } else {
+      parent.style.display = "none";
+    }
+  });
 
-    console.log(list);
+  console.log(list);
 }
 
 // supprimer la ligne
 function deleteBtn(e) {
-    if (e.target.classList.contains("btn-danger")) {
-        const trParent = e.target.closest("tr");
-        if (confirm("Voulez-supprimer?"))
-            if (trParent) {
-                trParent.remove();
-            }
-    }
+  if (e.target.classList.contains("btn-danger")) {
+    const trParent = e.target.closest("tr");
+    if (confirm("Voulez-supprimer?"))
+      if (trParent) {
+        trParent.remove();
+      }
+  }
 }
 
 // supprimer la ligne avec parentElement
@@ -147,84 +161,91 @@ tableBody.addEventListener("click", deleteBtn);
 
 // trier option +/-
 document.querySelector("#sort").addEventListener("change", (e) => {
-    switch (e.target.value) {
-        case "option1":
-            return sortByPriorityASC();
-        case "option2":
-            return sortByPriorityDESC();
-        case "option3":
-            return sortByCheckboxON();
-        case "option4":
-            return sortByCheckboxOFF();
-        case "option5":
-            return sortByDateASC();
-        case "option6":
-            return sortByDateDESC();
-        case "annuler":
-            return reset();
-    }
+  switch (e.target.value) {
+    case "option1":
+      return sortByPriorityASC();
+    case "option2":
+      return sortByPriorityDESC();
+    case "option3":
+      return sortByCheckboxON();
+    case "option4":
+      return sortByCheckboxOFF();
+    case "option5":
+      return sortByDateASC();
+    case "option6":
+      return sortByDateDESC();
+    case "annuler":
+      return reset();
+  }
 });
 
 // tri par priorité +/-
 const sortByPriorityASC = () => {
-    // récupère toutes les todos et en les converties en items itérable
-    const children = [...tableBody.querySelectorAll("tr")];
+  // récupère toutes les todos et en les converties en items itérable
+  const children = [...tableBody.querySelectorAll("tr")];
 
-    // remplace tout les enfants de tableBody par le nouveau tri
-    tableBody.replaceChildren(...children.sort((a, b) => b.dataset.priority - a.dataset.priority));
+  // remplace tout les enfants de tableBody par le nouveau tri
+  tableBody.replaceChildren(
+    ...children.sort((a, b) => b.dataset.priority - a.dataset.priority)
+  );
 };
 
 // tri  par priorité -/+
 const sortByPriorityDESC = () => {
-    // récupère toutes les todos et en les converties en items itérable
-    const children = [...tableBody.querySelectorAll("tr")];
+  // récupère toutes les todos et en les converties en items itérable
+  const children = [...tableBody.querySelectorAll("tr")];
 
-    // remplace tout les enfants de tableBody par le nouveau tri
-    tableBody.replaceChildren(...children.sort((a, b) => a.dataset.priority - b.dataset.priority));
+  // remplace tout les enfants de tableBody par le nouveau tri
+  tableBody.replaceChildren(
+    ...children.sort((a, b) => a.dataset.priority - b.dataset.priority)
+  );
 };
 
 // tri par checkbox activé
 const sortByCheckboxON = () => {
-    const children = [...tableBody.querySelectorAll("tr")];
-    tableBody.replaceChildren(...children.sort((a, b) => {
-            const checkboxA = a.querySelector('input[type="checkbox"]');
-            const checkboxB = b.querySelector('input[type="checkbox"]');
-            return checkboxB.checked - checkboxA.checked;
-        })
-    );
+  const children = [...tableBody.querySelectorAll("tr")];
+  tableBody.replaceChildren(
+    ...children.sort((a, b) => {
+      const checkboxA = a.querySelector('input[type="checkbox"]');
+      const checkboxB = b.querySelector('input[type="checkbox"]');
+      return checkboxB.checked - checkboxA.checked;
+    })
+  );
 };
 
 // tri par checkbox désactivé
 const sortByCheckboxOFF = () => {
-    const children = [...tableBody.querySelectorAll("tr")];
-    tableBody.replaceChildren(...children.sort((a, b) => {
-            const checkboxA = a.querySelector('input[type="checkbox"]');
-            const checkboxB = b.querySelector('input[type="checkbox"]');
-            return checkboxA.checked - checkboxB.checked;
-        })
-    );
+  const children = [...tableBody.querySelectorAll("tr")];
+  tableBody.replaceChildren(
+    ...children.sort((a, b) => {
+      const checkboxA = a.querySelector('input[type="checkbox"]');
+      const checkboxB = b.querySelector('input[type="checkbox"]');
+      return checkboxA.checked - checkboxB.checked;
+    })
+  );
 };
 
 // tri par date +/-
 const sortByDateASC = () => {
-    const children = [...tableBody.querySelectorAll("tr")];
-    tableBody.replaceChildren(
-        ...children.sort((a, b) => {
-            const dateA = +a.dataset.date;
-            const dateB = +b.dataset.date;
-            return dateB - dateA;
-        })
-    );
+  const children = [...tableBody.querySelectorAll("tr")];
+  tableBody.replaceChildren(
+    ...children.sort((a, b) => {
+      const dateA = +a.dataset.date;
+      const dateB = +b.dataset.date;
+      console.log(dateA);
+      return dateB - dateA;
+    })
+  );
 };
 
 // tri par date -/+
 const sortByDateDESC = () => {
-    const children = [...tableBody.querySelectorAll("tr")];
-    tableBody.replaceChildren(
-        ...children.sort((a, b) => {
-            const dateA = +a.dataset.date;
-            const dateB = +b.dataset.date;
-            return dateA - dateB;
-        })
-    );
+  const children = [...tableBody.querySelectorAll("tr")];
+  tableBody.replaceChildren(
+    ...children.sort((a, b) => {
+      const dateA = +a.dataset.date;
+      const dateB = +b.dataset.date;
+      return dateA - dateB;
+    })
+  );
 };
